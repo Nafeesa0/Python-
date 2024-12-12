abc4948be62b7675a2028765a2108b4de5d930fa
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2024-12-12 14:52:27 +0530

    Add files via upload

---
+++ december/package2/arithmetic.py
@@ -0,0 +1,17 @@
+# math_operations/basic_operations
+# arithmetic.py
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b!=0:
+        return a/b
+    else:
+        return "division by 0 is not allowed"
