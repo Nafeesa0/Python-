9b0f7d38bcb710a61f96ad63e4396b05f74a385c
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-01-19 19:43:56 +0530

    Update rectangle.py

--- december/rectangle.py
+++ december/rectangle.py
@@ -1,23 +1,33 @@
 class Rectangle():
-           def __init__(self, l, w):
-               self.length = l
-               self.breadth  = w
+    def __init__(self, l, w):
+        self.length = l
+        self.breadth = w
 
-           def rectangle_area(self):
-               return self.length*self.breadth
+    def rectangle_area(self):
+        return self.length * self.breadth
 
-           def rectangle_peri(self):
-               return 2* (self.length+self.breadth)
+    def rectangle_peri(self):
+        return 2 * (self.length + self.breadth)
 
-rectangle1 = Rectangle(12, 10)
-rectangle2 = Rectangle(6, 5)
 
-print("Area1 = ",rectangle1.rectangle_area())
-print("Area2 = ",rectangle2.rectangle_area())
-print("Perimeter1 = ",rectangle1.rectangle_peri())
-print("Perimeter2 = ",rectangle2.rectangle_peri())
+# Input for the first rectangle
+length1 = float(input("Enter the length of the first rectangle: "))
+breadth1 = float(input("Enter the breadth of the first rectangle: "))
+rectangle1 = Rectangle(length1, breadth1)
 
-if (rectangle1.rectangle_area()==rectangle2.rectangle_area()):
+# Input for the second rectangle
+length2 = float(input("Enter the length of the second rectangle: "))
+breadth2 = float(input("Enter the breadth of the second rectangle: "))
+rectangle2 = Rectangle(length2, breadth2)
+
+# Calculations and outputs
+print("Area of Rectangle 1 =", rectangle1.rectangle_area())
+print("Area of Rectangle 2 =", rectangle2.rectangle_area())
+print("Perimeter of Rectangle 1 =", rectangle1.rectangle_peri())
+print("Perimeter of Rectangle 2 =", rectangle2.rectangle_peri())
+
+# Comparing areas
+if rectangle1.rectangle_area() == rectangle2.rectangle_area():
     print("Areas are Equal")
 else:
-    print("Areas are not Equal") 
+    print("Areas are not Equal")
